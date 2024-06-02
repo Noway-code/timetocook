361b11d67ef05beff3c6234b271e3fa2d8265af6
noway <dragonfury1928@gmail.com>
2024-06-02 16:58:35 -0600

    Add: Git activity and home page changes
    Cool logo button created

--- src/app/page.js
+++ src/app/page.js
@@ -3,66 +3,95 @@
 import useAnimateOnScroll from './components/useAnimateOnScroll';
 import Section from './components/Section';
 import Link from "next/link";
+import {useState} from "react";
 
 export default function Home() {
 	useAnimateOnScroll();
+	const [isHovered, setIsHovered] = useState(false);
 
-	return (
-		<div className="bg-gray-700 mx-auto min-h-screen flex flex-col items-start justify-center space-y-10 px-10">
-			<Section className="h-screen flex flex-col justify-center" delay={200}>
-				<h1 className="font-black text-6xl">
-					Hells to the Yeah
-				</h1>
-				<p className="text-gray-500 text-lg">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptas eius explicabo suscipit, nemo sapiente
-					dignissimos, ipsum illum minus ab itaque accusamus reprehenderit. Quis fuga iusto saepe nobis sunt a. Lorem, ipsum dolor
-					sit amet consectetur adipisicing elit. Optio ut nulla quos nemo nesciunt enim officiis laudantium, neque labore. Officia
-					illo obcaecati, impedit distinctio dicta laudantium mollitia quam nihil quisquam!
+	const handleMouseEnter = () => {
+		setIsHovered(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsHovered(false);
+	};
+
+	return (<div className="mx-auto flex min-h-screen flex-col items-start justify-center bg-gray-700 px-10 space-y-10">
+		<Section className="flex h-screen flex-col items-center justify-center" delay={200}>
+			<h1 className="text-7xl font-black">
+				Hello, i&apos;m Camilo
+			</h1>
+			<div>
+				<p className="text-2xl text-gray-200">
+					I&apos;m a software engineer who loves building cool stuff, from web apps to mobile games. Right now,
+					I&apos;m juggling a bunch of personal projects and working as a software developer intern at{' '}
+					<Link href="/about">
+						<span
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+							className={`inline-block p-1 rounded-md cursor-pointer font-semibold ${isHovered ? 'bg-red-600 text-white' : 'bg-white text-red-500'}`}
+						>
+            Thermo Fisher
+            <span className={`font-light ${isHovered ? 'text-white' : 'text-black'}`}> Scientific</span>
+          </span>
+					</Link>.
 				</p>
 
-				<div className="mt-10 sm:flex sm:justify-center lg:justify-start">
-					<div className="rounded-md shadow">
-						<Link href="/about">
-							<div className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-								Get started
-							</div>
-						</Link>
-					</div>
-					<div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-						<a href="#"
-						   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
-							Live demo
-						</a>
-					</div>
+				<p className="mt-5 text-gray-200">
+					I&apos;m also a student at UCF, always on the lookout for new ways to learn and level up my skills.
+				</p>
+
+			</div>
+
+
+			<div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+				<div className="rounded-md shadow">
+					<Link href="/about">
+						<div
+							className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:shadow-outline-indigo focus:border-indigo-700 focus:outline-none md:px-10 md:py-4 md:text-lg">
+							Get started
+						</div>
+					</Link>
 				</div>
-			</Section>
+				<div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+					<a href="#"
+					   className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out hover:text-indigo-500 focus:shadow-outline-indigo focus:border-indigo-300 focus:outline-none md:px-10 md:py-4 md:text-lg">
+						Live demo
+					</a>
+				</div>
+			</div>
+		</Section>
 
-			<Section className="h-screen flex flex-col justify-center" delay={400}>
-				<h2 className="font-black text-5xl">
-					More Content Here
-				</h2>
-				<p className="text-gray-500 text-lg">
-					Additional lorem ipsum text to fill out the page and test the scrolling animation effect. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</p>
-			</Section>
+		<Section className="flex h-screen flex-col justify-center" delay={400}>
+			<h2 className="text-5xl font-black">
+				More Content Here
+			</h2>
+			<p className="text-lg text-gray-500">
+				Additional lorem ipsum text to fill out the page and test the scrolling animation effect. Lorem ipsum dolor sit amet, consectetur
+				adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</p>
+		</Section>
 
-			<Section className="h-screen flex flex-col justify-center" delay={600}>
-				<h2 className="font-black text-5xl">
-					Even More Content
-				</h2>
-				<p className="text-gray-500 text-lg">
-					More placeholder text to ensure that the page has enough content to scroll and trigger the animation effect. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-				</p>
-			</Section>
+		<Section className="flex h-screen flex-col justify-center" delay={600}>
+			<h2 className="text-5xl font-black">
+				Even More Content
+			</h2>
+			<p className="text-lg text-gray-500">
+				More placeholder text to ensure that the page has enough content to scroll and trigger the animation effect. Lorem ipsum dolor sit
+				amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+			</p>
+		</Section>
 
-			<Section className="h-screen flex flex-col justify-center" delay={800}>
-				<h2 className="font-black text-5xl">
-					Final Section
-				</h2>
-				<p className="text-gray-500 text-lg">
-					The final section with additional text to ensure the scrolling animation effect can be observed as you move down the page. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
-				</p>
-			</Section>
-		</div>
-	);
+		<Section className="flex h-screen flex-col justify-center" delay={800}>
+			<h2 className="text-5xl font-black">
+				Final Section
+			</h2>
+			<p className="text-lg text-gray-500">
+				The final section with additional text to ensure the scrolling animation effect can be observed as you move down the page. Lorem
+				ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
+			</p>
+		</Section>
+	</div>);
 }
