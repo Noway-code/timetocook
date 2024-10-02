3fd6a61c34ccdeb9c434236a346c48cd5fc42b84
noway <dragonfury1928@gmail.com>
2024-10-01 20:58:22 -0400

    Change metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,15 @@
 import './globals.css';
 import Nav from "@/app/components/Nav";
-import { Analytics } from "@vercel/analytics/react"
+import {Analytics} from "@vercel/analytics/react"
 
 export const metadata = {
-  title: "Noway Portfolio",
+	title: "Noway Portfolio!",
   description: "Camilo Alvarez-Velez",
+	image: "IMG_5266.JPEG",
+	url: "https://nowaycode.com",
+	type: "website",
+	site_name: "Noway Portfolio!",
+	locale: "en_US",
 };
 
 export default function RootLayout({ children }) {
