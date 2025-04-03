1d7e7cecbb598e51e66199acd3f31cf61cc88c07
noway-code <dragonfury1928@gmail.com>
2025-04-02 20:00:17 -0400

    Add ladybug and highlight it

--- src/app/activity/page.js
+++ src/app/activity/page.js
@@ -8,195 +8,196 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Nav from "@/app/components/Nav";
-import { Element, Link as ScrollLink } from "react-scroll";
+import {Element, Link as ScrollLink} from "react-scroll";
 import Projects from "@/app/data/projects";
 import SkillsSection from "@/app/components/SkillsSection";
 
 export default function Page() {
-	useAnimateOnScroll();
+    useAnimateOnScroll();
 
-	const sliderSettings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		adaptiveHeight: true,
-		centerMode: true,
-		centerPadding: "0",
-	};
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        adaptiveHeight: true,
+        centerMode: true,
+        centerPadding: "0",
+    };
 
-	return (
-		<div className="bg-gradient-to-br from-blue-900 to-purple-950 min-h-screen">
-			{/* Navigation */}
-			<Element name="nav">
-				<Nav />
-			</Element>
+    return (
+        <div className="bg-gradient-to-br from-blue-900 to-purple-950 min-h-screen">
+            {/* Navigation */}
+            <Element name="nav">
+                <Nav/>
+            </Element>
 
-			<div className="container mx-auto px-6 py-12 flex flex-col space-y-16">
-				{/* Recent Activity Section */}
-				<Element name="recent-activity">
-					<Section
-						className="flex flex-col items-center text-center justify-center min-h-screen"
-						delay={200}
-					>
-						<h1 className="font-black text-6xl mb-4 drop-shadow-lg">
-							Recent Activity
-						</h1>
-						<p className="text-2xl mb-8 max-w-2xl">
-							Get an overview of my latest contributions, projects, and coding
-							adventures.
-						</p>
+            <div className="container mx-auto px-6 py-12 flex flex-col space-y-16">
+                {/* Recent Activity Section */}
+                <Element name="recent-activity">
+                    <Section
+                        className="flex flex-col items-center text-center justify-center min-h-screen"
+                        delay={200}
+                    >
+                        <h1 className="font-black text-6xl mb-4 drop-shadow-lg">
+                            Recent Activity
+                        </h1>
+                        <p className="text-2xl mb-8 max-w-2xl">
+                            Get an overview of my latest contributions, projects, and coding
+                            adventures.
+                        </p>
 
-						{/* Jump-to Links */}
-						<div className="flex flex-col md:flex-row items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-2xl p-6 space-y-4 md:space-y-0 md:space-x-6 font-bold shadow-xl">
-							<span className="text-amber-50 text-2xl">Jump to:</span>
-							<ul className="flex flex-wrap justify-center gap-4">
-								<li className="bg-blue-500 px-4 py-2 rounded-lg">
-									<ScrollLink
-										to="recent-activity"
-										smooth={true}
-										duration={500}
-										className="cursor-pointer"
-									>
-										Recent Activity
-									</ScrollLink>
-								</li>
-								<li className="bg-blue-500 px-4 py-2 rounded-lg">
-									<ScrollLink
-										to="projects"
-										smooth={true}
-										duration={500}
-										className="cursor-pointer"
-									>
-										Projects
-									</ScrollLink>
-								</li>
-								<li className="bg-blue-500 px-4 py-2 rounded-lg">
-									<ScrollLink
-										to="github-activity"
-										smooth={true}
-										duration={500}
-										className="cursor-pointer"
-									>
-										GitHub Activity
-									</ScrollLink>
-								</li>
-								<li className="bg-blue-500 px-4 py-2 rounded-lg">
-									<ScrollLink
-										to="tech-stack"
-										smooth={true}
-										duration={500}
-										className="cursor-pointer"
-									>
-										Tech Stack
-									</ScrollLink>
-								</li>
-							</ul>
-						</div>
-					</Section>
-				</Element>
+                        {/* Jump-to Links */}
+                        <div
+                            className="flex flex-col md:flex-row items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-2xl p-6 space-y-4 md:space-y-0 md:space-x-6 font-bold shadow-xl">
+                            <span className="text-amber-50 text-2xl">Jump to:</span>
+                            <ul className="flex flex-wrap justify-center gap-4">
+                                <li className="bg-blue-500 px-4 py-2 rounded-lg">
+                                    <ScrollLink
+                                        to="recent-activity"
+                                        smooth={true}
+                                        duration={500}
+                                        className="cursor-pointer"
+                                    >
+                                        Recent Activity
+                                    </ScrollLink>
+                                </li>
+                                <li className="bg-blue-500 px-4 py-2 rounded-lg">
+                                    <ScrollLink
+                                        to="projects"
+                                        smooth={true}
+                                        duration={500}
+                                        className="cursor-pointer"
+                                    >
+                                        Projects
+                                    </ScrollLink>
+                                </li>
+                                <li className="bg-blue-500 px-4 py-2 rounded-lg">
+                                    <ScrollLink
+                                        to="github-activity"
+                                        smooth={true}
+                                        duration={500}
+                                        className="cursor-pointer"
+                                    >
+                                        GitHub Activity
+                                    </ScrollLink>
+                                </li>
+                                <li className="bg-blue-500 px-4 py-2 rounded-lg">
+                                    <ScrollLink
+                                        to="tech-stack"
+                                        smooth={true}
+                                        duration={500}
+                                        className="cursor-pointer"
+                                    >
+                                        Tech Stack
+                                    </ScrollLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </Section>
+                </Element>
 
-				{/* Projects Section */}
-				<Element name="projects">
-					<Section className="w-full" delay={300}>
-						<h2 className="text-4xl font-bold mb-6 text-center">
-							Highlighted Projects
-						</h2>
-						<div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-							{Projects.map((project) => (
-								<div
-									key={project.id}
-									className="bg-gray-800 p-6 rounded-lg shadow-2xl hover:shadow-3xl transform hover:-translate-y-2 transition duration-300 flex flex-col"
-								>
-									<h3 className="text-2xl font-bold mb-4">{project.title}</h3>
-									{/* Image Carousel */}
-									<Slider {...sliderSettings}>
-										{project.images.map((image, index) => (
-											<div key={index}>
-												<img
-													src={image}
-													alt={`Screenshot of ${project.title}`}
-													className="w-full object-cover object-center rounded-lg max-h-80"
-												/>
-											</div>
-										))}
-									</Slider>
-									<p className="text-gray-400 mt-4 flex-grow">
-										{project.description}
-									</p>
-									{/* Tech Stack */}
-									<div className="flex flex-wrap justify-start gap-2 mt-4">
-										{project.techStack.map((tech, index) => (
-											<span
-												key={index}
-												className="px-3 py-1 bg-gray-700 text-white rounded-full"
-											>
+                {/* Projects Section */}
+                <Element name="projects">
+                    <Section className="w-full" delay={300}>
+                        <h2 className="text-4xl font-bold mb-6 text-center">
+                            Highlighted Projects
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                            {Projects.map((project) => (
+                                <div
+                                    key={project.id}
+                                    className={`bg-gray-800 p-6 rounded-lg shadow-2xl hover:shadow-3xl transform hover:-translate-y-2 transition duration-300 flex flex-col ${project.highlighted ? 'border-4 border-yellow-500' : ''}`}
+                                >
+                                    <h3 className="text-2xl font-bold mb-4">{project.title}</h3>
+                                    {/* Image Carousel */}
+                                    <Slider {...sliderSettings}>
+                                        {project.images.map((image, index) => (
+                                            <div key={index}>
+                                                <img
+                                                    src={image}
+                                                    alt={`Screenshot of ${project.title}`}
+                                                    className="w-full object-cover object-center rounded-lg max-h-90"
+                                                />
+                                            </div>
+                                        ))}
+                                    </Slider>
+                                    <p className="text-gray-400 mt-4 flex-grow">
+                                        {project.description}
+                                    </p>
+                                    {/* Tech Stack */}
+                                    <div className="flex flex-wrap justify-start gap-2 mt-4">
+                                        {project.techStack.map((tech, index) => (
+                                            <span
+                                                key={index}
+                                                className="px-3 py-1 bg-gray-700 text-white rounded-full"
+                                            >
                         {tech}
                       </span>
-										))}
-									</div>
-									{/* Links */}
-									<div className="flex flex-wrap justify-start gap-2 mt-4">
-										{project.links.map((link, index) => (
-											<a
-												key={index}
-												href={link.url}
-												className="inline-block px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-500 transition-colors duration-300"
-												target="_blank"
-												rel="noopener noreferrer"
-											>
-												View {link.name}
-											</a>
-										))}
-									</div>
-								</div>
-							))}
-						</div>
-					</Section>
-				</Element>
+                                        ))}
+                                    </div>
+                                    {/* Links */}
+                                    <div className="flex flex-wrap justify-start gap-2 mt-4">
+                                        {project.links.map((link, index) => (
+                                            <a
+                                                key={index}
+                                                href={link.url}
+                                                className="inline-block px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-500 transition-colors duration-300"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                View {link.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </Section>
+                </Element>
 
-				{/* GitHub Activity Section */}
-				<Element name="github-activity">
-					<Section className="w-full" delay={400}>
-						<div className="max-w-7xl mx-auto">
-							<h2 className="text-4xl font-bold mb-6 text-center">
-								GitHub Contributions
-							</h2>
-							<div className="bg-gray-800 rounded-2xl p-6 mb-8 shadow-xl">
-								<GitHubCalendar
-									username="Noway-code"
-									blockMargin={8}
-									blockSize={15}
-									fontSize={16}
-								/>
-							</div>
-							<div className="bg-gray-800 rounded-2xl p-6 mb-8 shadow-xl">
-								<h3 className="text-3xl mb-4">Recent Commits</h3>
-								<GitHubFeed />
-							</div>
-						</div>
-					</Section>
-				</Element>
+                {/* GitHub Activity Section */}
+                <Element name="github-activity">
+                    <Section className="w-full" delay={400}>
+                        <div className="max-w-7xl mx-auto">
+                            <h2 className="text-4xl font-bold mb-6 text-center">
+                                GitHub Contributions
+                            </h2>
+                            <div className="bg-gray-800 rounded-2xl p-6 mb-8 shadow-xl">
+                                <GitHubCalendar
+                                    username="Noway-code"
+                                    blockMargin={8}
+                                    blockSize={15}
+                                    fontSize={16}
+                                />
+                            </div>
+                            <div className="bg-gray-800 rounded-2xl p-6 mb-8 shadow-xl">
+                                <h3 className="text-3xl mb-4">Recent Commits</h3>
+                                <GitHubFeed/>
+                            </div>
+                        </div>
+                    </Section>
+                </Element>
 
-				<SkillsSection />
+                <SkillsSection/>
 
-				{/* Footer */}
-				<footer className="w-full text-center py-8">
-					<p className="text-lg">
-						Feel free to explore more about my work on GitHub and connect with me!
-					</p>
-					<ScrollLink
-						to="nav"
-						smooth={true}
-						duration={500}
-						className="inline-block mt-4 px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-500 transition-colors duration-300 cursor-pointer"
-					>
-						Back to Top
-					</ScrollLink>
-				</footer>
-			</div>
-		</div>
-	);
+                {/* Footer */}
+                <footer className="w-full text-center py-8">
+                    <p className="text-lg">
+                        Feel free to explore more about my work on GitHub and connect with me!
+                    </p>
+                    <ScrollLink
+                        to="nav"
+                        smooth={true}
+                        duration={500}
+                        className="inline-block mt-4 px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-500 transition-colors duration-300 cursor-pointer"
+                    >
+                        Back to Top
+                    </ScrollLink>
+                </footer>
+            </div>
+        </div>
+    );
 }
--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -1,5 +1,15 @@
 const projects = [{
     id: 1,
+    title: 'Ladybug',
+    description: 'Utilizes LLM\'s and novel GUI data techniques to automate bug localization in your repositories. Integrates with GitHub to trigger analysis on new issues and provides bug localization results.',
+    links: [{
+        name: 'GitHub', url: 'https://github.com/Noway-code/ladybug'
+    }],
+    images: ['/ladybug/ppt.png', '/ladybug/red_wing.png', '/ladybug/report.png'],
+    highlighted: true,
+    techStack: ['Flask', 'Python', 'Node.js', 'Probot'],
+}, {
+    id: 2,
     title: 'TypeAlong',
     description: 'Sleek typing game that transforms EPUB books into engaging touch typing challenges, built with SvelteKit and TypeScript.',
     links: [{
@@ -10,7 +20,7 @@ const projects = [{
     images: ['/typealong/home-page.png', '/typealong/home-page-nord.png', '/typealong/book-toc.png', '/typealong/practice-type.png', '/typealong/themes.png'],
     techStack: ['SvelteKit', 'TypeScript', 'Node.js', 'EPUB.js', 'Python', 'Vercel'],
 }, {
-    id: 2,
+    id: 3,
     title: 'CreditShield',
     description: 'Advanced fraud detection system leveraging ML, synthetic data, and blockchain to secure real-time banking transactions. Winner of 1st place at KnightHacks 24.',
     links: [{
@@ -21,7 +31,7 @@ const projects = [{
     images: ['/creditshield/pic3.jpg', '/creditshield/pic1.jpg', '/creditshield/pic2.jpg'],
     techStack: ['Python', 'React', 'FastAPI', 'ML', 'Ethereum', 'Mongodb'],
 }, {
-    id: 3,
+    id: 4,
     title: 'AStarFriends',
     description: 'Effortlessly organize carpools with optimized routes. Create parties, assign drivers, and set destinations for a hassle-free ride.',
     links: [{
@@ -30,7 +40,7 @@ const projects = [{
     images: ['/astar/selectmap.png', '/astar/routes.png', '/astar/start.png'],
     techStack: ['Python', 'React', 'FastAPI', 'Google OR-Tools', 'OSRM API', 'SQLite'],
 }, {
-    id: 4,
+    id: 5,
     title: 'Pulse Pal',
     description: 'Streamlines patient management and remote disease detection using rPPG. Awarded 3rd place at Hacklytics 24 Elevance Challenge.',
     links: [{
@@ -41,7 +51,7 @@ const projects = [{
     images: ['pulse/web.png', '/pulse/lockin.png', 'pulse/mobile.png'],
     techStack: ['Python', 'React', 'React Native', 'Flask', 'OpenCV', 'Jupyter'],
 }, {
-    id: 5,
+    id: 6,
     title: 'Town Trekkr',
     description: 'Engaging community game where you guess photo locations, build towns, and share snapshots. Dive into the adventure!',
     links: [{
@@ -50,7 +60,7 @@ const projects = [{
     images: ['/town/play.png', '/town/stats.png', '/town/webmenu.png'],
     techStack: ['React', 'Tailwind CSS', 'Node.js', 'Express', 'MongoDB'],
 }, {
-    id: 6,
+    id: 7,
     title: 'Portfolio',
     description: 'Explore my projects and blog in this portfolio, crafted with Next.js, Tailwind CSS, and Vercel.',
     links: [{
@@ -59,7 +69,7 @@ const projects = [{
     images: ['/port/obsidian.png', '/port/mirror.png'],
     techStack: ['React', 'Tailwind CSS', 'Next.js', 'Vercel', 'GitHub API', 'Obsidian', 'Cloudflare'],
 }, {
-    id: 7,
+    id: 8,
     title: 'Freelance - REV Group Orlando',
     description: 'Python Tkinter GUI that automates truck preparation, slashing task time from 10 minutes to under 1 minute and minimizing human error.',
     links: [{
@@ -68,7 +78,7 @@ const projects = [{
     images: ['/REV.png'],
     techStack: ['Python', 'Tkinter', 'Windows', 'Pyinstaller'],
 }, {
-    id: 8,
+    id: 9,
     title: 'Phone Home',
     description: 'Web-based contact manager with a space alien twist.',
     links: [{
@@ -77,13 +87,13 @@ const projects = [{
     images: ['/phonehome/contacts.png', '/phonehome/login.png'],
     techStack: ['Bootstrap', 'JS/HTML/CSS', 'Linux', 'Apache', 'MySQL', 'PHP', 'DigitalOcean'],
 }, {
-    id: 9,
+    id: 10,
     title: 'dotfiles',
     description: 'A curated collection of custom Linux dotfiles and automation scripts that boost system efficiency.',
     links: [{
         name: 'GitHub', url: 'https://github.com/Noway-code/dotfiles'
     }],
-    images: ['/dotfiles/rice0.png','/dotfiles/rice2.png', '/dotfiles/rice.png'],
+    images: ['/dotfiles/rice0.png', '/dotfiles/rice2.png', '/dotfiles/rice.png'],
     techStack: ['Bash', 'Neovim', 'Git', 'Linux', 'Arch', 'btw']
 }];
 
